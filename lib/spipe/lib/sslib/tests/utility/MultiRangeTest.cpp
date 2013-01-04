735e1509c67435d6d9d82724a6846b9d703f20d6
Martin Uhrin <martin.uhrin@gmail.com>
2013-01-04 19:12:16 +0000

    Starting work on MultiRange

---
+++ lib/spipe/lib/sslib/tests/utility/MultiRangeTest.cpp
@@ -0,0 +1,29 @@
+/*
+ * MultiRangeTest.cpp
+ *
+ *  Created on: Aug 18, 2011
+ *      Author: Martin Uhrin
+ */
+
+// INCLUDES //////////////////////////////////
+#include "sslibtest.h"
+
+#include <iostream>
+
+#include <boost/foreach.hpp>
+#include <boost/iterator/iterator_categories.hpp>
+
+#include <utility/MultiRange.h>
+
+namespace ssu = ::sstbx::utility;
+
+BOOST_AUTO_TEST_CASE(IntegerMultiIndexTest)
+{
+  typedef ssu::MultiRange<int, ::boost::forward_traversal_tag, int, ::std::ptrdiff_t>::AnyIterator AnyIterator;
+
+  ssu::MultiRange<int, ::boost::forward_traversal_tag, int, ::std::ptrdiff_t> range;
+
+  ::std::vector<int> testVector;
+
+  range.pushBack(testVector);
+}
