09ce979aa4a88c3e4b56a77201e08c9cfd132fb1
Martin Uhrin <martin.uhrin@gmail.com>
2013-04-22 11:56:17 +0100

    Added Iron to list of elements.

--- lib/spipe/lib/sslib/include/common/AtomSpeciesId.h
+++ lib/spipe/lib/sslib/include/common/AtomSpeciesId.h
@@ -42,6 +42,7 @@ public:
   static const Enumerator SI;
   static const Enumerator P;
   static const Enumerator CL;
+  static const Enumerator FE;
   static const Enumerator AS;
   static const Enumerator CUSTOM_1;
   static const Enumerator CUSTOM_2;
