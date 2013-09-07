94d0d6f0c199c102b4d33eabc8ca141ca359ba5b
Martin Uhrin <martin.uhrin@gmail.com>
2013-09-07 22:12:04 +0100

    Squashed 'lib/spipe/lib/pipelib/' changes from 0c04df3..ad05a98
    ad05a98 First implementation of boost threaded engine.
    21817b4 Started work on boost threaded engine.  Switching to pipelib repo to continue.
    
    git-subtree-dir: lib/spipe/lib/pipelib
    git-subtree-split: ad05a986514512839c480f544837f11d4226d2e4

--- include/pipelib/pipelib.h
+++ include/pipelib/pipelib.h
@@ -1,13 +1,15 @@
 #ifndef PIPELIB_H
 #define PIPELIB_H
 
+#include "pipelib/PipelibConfig.h"
 #include "pipelib/Pipeline.h"
 #include "pipelib/Block.h"
+#include "pipelib/BoostThreadEngine.h"
 #include "pipelib/PipeBlock.h"
 #include "pipelib/StartBlock.h"
 #include "pipelib/SimpleBarrier.h"
 #include "pipelib/SerialEngine.h"
 #include "pipelib/Sinks.h"
-
+#include "pipelib/Types.h"
 
 #endif  // #ifndef PIPELIB_H
