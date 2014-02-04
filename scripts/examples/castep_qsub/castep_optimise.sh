9f72267e1ba717f2b2be7d380f60d18c0d74bb6a
Martin Uhrin <martin.uhrin.10@ucl.ac.uk>
2014-02-04 15:00:08 +0000

    Updated linux scripts and build files so that they can now be installed wit the install_scripts target.

--- scripts/examples/castep_qsub/castep_optimise.sh
+++ scripts/examples/castep_qsub/castep_optimise.sh
@@ -5,6 +5,6 @@ export REMOTE_HOST=muhrincp@login.hector.ac.uk
 export REMOTE_WORK_DIR=work/runs/remote
 export JOB_FILE=graphene.job
 
-castep_qsub_optimiser.sh $1 $2
+castep_qsub_optimiser $1 $2
 
 exit $?
